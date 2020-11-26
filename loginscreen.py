04416076d738d674e7d895b7f1b98f644d309279
rushikesh552000 <rishikeshshinde377@gmail.com>
2020-11-26 16:22:11 +0530

    loginscreen with kivymd

---
+++ loginscreen.py
@@ -0,0 +1,6 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.button import MDRoundFlatButton
+
+class LogInScreen(MDScreen):
+    pass
---
+++ main.py
@@ -0,0 +1,17 @@
+from kivymd.app import MDApp
+from kivy import utils
+
+from kivy.core.window import Window
+
+Window.size =(350,500)
+
+
+
+
+class MainApp(MDApp):
+    def build(self):
+        return
+
+
+if __name__ == '__main__':
+    MainApp().run()
